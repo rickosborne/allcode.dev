4ec9d39098653eeda6883f795480a0d1bbcdd940
Rick Osborne <ricko@rickosborne.org>
2020-12-29 08:27:31 -0800

    Production profile

--- allcode-dev/src/environments/environment.prod.ts
+++ allcode-dev/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDLKjofTVtCwZfVKEYnhy9E-Go7nBQqgtw',
+    authDomain: 'allcode-dev.firebaseapp.com',
+    projectId: 'allcode-dev',
+    storageBucket: 'allcode-dev.appspot.com',
+    messagingSenderId: '661997544228',
+    appId: '1:661997544228:web:a9647f9b39ec149e37c9b1'
+  },
 };
