aa0f2745892b30ddb67acbf7eb4596eeaafb17f5
Rick Osborne <ricko@rickosborne.org>
2021-02-04 18:11:40 -0800

    Get rid of Angular

--- allcode-dev/src/app/HasLessonRefs.ts
+++
@@ -1,6 +0,0 @@
-import {QueryList} from "@angular/core";
-import {LessonRefComponent} from "./lesson-ref/lesson-ref.component";
-
-export interface HasLessonRefs {
-  lessonRefs: QueryList<LessonRefComponent>;
-}
--- allcode-dev/src/app/HasSourceRefs.ts
+++
@@ -1,6 +0,0 @@
-import {QueryList} from "@angular/core";
-import {SourceRefComponent} from "./source-ref/source-ref.component";
-
-export interface HasSourceRefs {
-  sourceRefs: QueryList<SourceRefComponent>;
-}
